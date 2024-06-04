cca28a4d4f1ed4cdac57e00dd6e2d55c0ba4ab2c
kbladewht <w00110601111@163.com>
2024-06-05 07:53:53 +0800

    Update board.h

--- src/boards/feather_nrf52833_pivot/board.h
+++ src/boards/feather_nrf52833_pivot/board.h
@@ -42,7 +42,7 @@
 /*------------------------------------------------------------------*/
 /* BUTTON
  *------------------------------------------------------------------*/
-#define BUTTONS_NUMBER        0
+#define BUTTONS_NUMBER        2
 #define BUTTON_1              _PINNUM(1, 02)
 #define BUTTON_2              _PINNUM(0, 10)
 #define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
